67742ec853486c270c0628d6bcd9e4f679602e9c
mateo-a <alexander.urrego@gmail.com>
2020-04-29 18:12:31 -0500

    [FIX] Removing option to pass in user.py

--- models/user.py
+++ models/user.py
@@ -6,7 +6,6 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,14 +26,4 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
-        if kwargs:
-            kwargs.pop('password', None)
-            User.set_password_md5(self, pwd)
         super().__init__(*args, **kwargs)
-
-    def set_password_md5(self, pwd):
-        """ Method to set the pwd into MD5 algorithm """
-        new_pwd = hashlib.md5()
-        new_pwd.update(pwd.encode('utf-8'))
-        encrypted_pwd = new_pwd.hexdigest()
-        setattr(self, 'password', encrypted_pwd)
