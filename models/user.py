5ed641ad121082717d1f5baaeb113a1554a98011
mateo-a <alexander.urrego@gmail.com>
2020-04-29 19:23:34 -0500

    [FIX] Task 15

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -23,6 +24,12 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
+
+    def __setattr__(self, name, value):
+        """ Method to set the pwd into MD5 algorithm """
+        if name == 'password':
+            value = hashlib.md5(value.encode()).hexdigest()
+        super.__setattr__(self, name, value)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
